6f6d9d58300b59dacdb86054e37268727e597cbf
h-kato0610 <h-kato0610@gmail.com>
2022-11-10 06:17:42 +0900

    update error process

--- try-01-casesar-cipher/src/main.rs
+++ try-01-casesar-cipher/src/main.rs
@@ -3,8 +3,12 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() != {
+        panic("\n Input your [OriginalText] [Shift Num]");
+    }
+
     let in_text = &args[1];
-    let shift: i16 = args[2].parse().unwrap();
+    let shift: i16 = args[2].trim().parse().except("Input Your Integer");
 
     let encode_text = encode(in_text, shift);
     println!("{} -> {}", in_text, encode_text);
